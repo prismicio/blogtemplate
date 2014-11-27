fc2063d4e6d2863731dcccf32ba2baf310e510e6
Erwan Loisant <eloisant@gmail.com>
2014-11-27 17:38:34 +0100

    List pages on index

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require 'vendor/autoload.php';
 require_once 'config.php';
 
+use Prismic\Api;
+use Prismic\Predicates;
+
 $app = new \Slim\Slim();
 
 // Page
@@ -16,7 +19,18 @@ $app->get('/:docid/:slug', function() {
 
 // Index
 $app->get('/', function() {
+    $api = Api::get(PRISMIC_URL);
+    $pages = $api->forms()
+        ->everything
+        ->query(Predicates::at("document.type", "page"))
+        ->ref($api->master())
+        ->submit();
     echo "Hello, world";
+    echo "<ul>";
+    foreach($pages->getResults() as $page) {
+        echo "<li>" . $page->getText("page.title") . "</li>";
+    }
 });
 
 $app->run();
+
