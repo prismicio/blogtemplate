aad9009a37c10002117ae4b0dff41f0227621849
Erwan Loisant <eloisant@gmail.com>
2014-12-04 17:11:55 +0100

    Start with twig

--- includes/theme.php
+++ includes/theme.php
@@ -2,6 +2,20 @@
 
 class Theme {
 
+    private static $twig;
+
+    private static function twig() {
+        if (!Theme::$twig) {
+            Twig_Autoloader::register();
+
+            $loader = new Twig_Loader_Filesystem(Theme::directory());
+            Theme::$twig = new Twig_Environment($loader, array(
+                // 'cache' => '/path/to/compilation_cache',
+            ));
+        }
+        return Theme::$twig;
+    }
+
     public static function directory() {
         return 'themes/' . PI_THEME;
     }
@@ -11,7 +25,16 @@ class Theme {
     }
 
     public static function render($name) {
-        include Theme::directory() . '/' . $name . '.php';
+        if (Theme::isWP()) {
+            include Theme::directory() . '/' . $name . '.php';
+        } else {
+            echo Theme::twig()->render($name . '.html');
+        }
+    }
+
+    private static function isWP() {
+        return file_exists('themes/' . PI_THEME . '/index.php');
     }
 
 }
+
