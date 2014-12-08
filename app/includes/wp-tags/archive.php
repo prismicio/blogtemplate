a6ce7c847372b966420d6aed4db5bdc3a005ce41
Erwan Loisant <eloisant@gmail.com>
2014-12-08 14:02:14 +0100

    Fix archive and add more tests

--- app/includes/wp-tags/archive.php
+++ app/includes/wp-tags/archive.php
@@ -1,33 +1,41 @@
 <?php
 
 function is_day() {
-    return State::$current_archive_date && State::$current_archive_date['day'];
+    global $WPGLOBAL;
+    $state = $WPGLOBAL['state'];
+    return $state->current_archive_date && $state->current_archive_date['day'];
 }
 
 function is_month() {
-    return State::$current_archive_date && !is_day() && State::$current_archive_date['month'];
+    global $WPGLOBAL;
+    $state = $WPGLOBAL['state'];
+    return $state->current_archive_date && !is_day() && $state->current_archive_date['month'];
 }
 
 function is_year() {
-    return State::$current_archive_date && !is_month() && !is_day();
+    global $WPGLOBAL;
+    $state = $WPGLOBAL['state'];
+    return $state->current_archive_date && !is_month() && !is_day();
 }
 
 function archive_date() {
+    global $WPGLOBAL;
+    $state = $WPGLOBAL['state'];
     if (is_year()) {
-        return State::$current_archive_date['year'];
+        return $state->current_archive_date['year'];
     }
     if (is_month()) {
-        $dt = DateTime::createFromFormat('!Y-m', State::$current_archive_date['year'] . '-' . State::$current_archive_date['month']);
+        $dt = DateTime::createFromFormat('!Y-m', $state->current_archive_date['year'] . '-' . $state->current_archive_date['month']);
         return $dt->format('F Y');
     }
     if (is_day()) {
-        echo State::$current_archive_date['year']
-        . '-' . State::$current_archive_date['month']
-        . '-' . State::$current_archive_date['day'];
+        echo $state->current_archive_date['year']
+        . '-' . $state->current_archive_date['month']
+        . '-' . $state->current_archive_date['day'];
         $dt = DateTime::createFromFormat('!Y-m-d',
-            State::$current_archive_date['year']
-            . '-' . State::$current_archive_date['month']
-            . '-' . State::$current_archive_date['day']);
+            $state->current_archive_date['year']
+            . '-' . $state->current_archive_date['month']
+            . '-' . $state->current_archive_date['day']);
         return $dt->format('F jS, Y');
     }
 }
