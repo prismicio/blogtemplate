c23b20bb6431f1ab5077f5c73cb28595b99b8895
Erwan Loisant <eloisant@gmail.com>
2014-12-08 14:35:01 +0100

    Author tests

--- tests/TwigTest.php
+++ tests/TwigTest.php
@@ -54,4 +54,14 @@ class TwigTests extends LocalWebTestCase
         $this->assertEquals(0, count($html('div.blog-post')));
     }
 
+    public function testAuthor()
+    {
+        $this->client->get('/author/VHiMRicAACcAHSaw/erwan-loisant');
+        $this->assertEquals(200, $this->client->response->status());
+
+        $html = str_get_dom($this->client->response->body());
+        $this->assertEquals(1, count($html('footer.blog-footer')));
+        $this->assertEquals(1, count($html('div.blog-post')));
+    }
+
 }
--- tests/WPTest.php
+++ tests/WPTest.php
@@ -59,4 +59,14 @@ class WPTest extends LocalWebTestCase
         $this->assertEquals(0, count($html('div.blog-post')));
     }
 
+    public function testAuthor()
+    {
+        $this->client->get('/author/VHiMRicAACcAHSaw/erwan-loisant');
+        $this->assertEquals(200, $this->client->response->status());
+
+        $html = str_get_dom($this->client->response->body());
+        $this->assertEquals(1, count($html('footer.blog-footer')));
+        $this->assertEquals(1, count($html('div.blog-post')));
+    }
+
 }
