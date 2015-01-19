c7230d124a2d21262238cfae02e6615707bc9e09
Erwan Loisant <eloisant@gmail.com>
2015-01-19 15:55:17 +0100

    400 Lato for the menu

--- app/themes/bootstrap/header.php
+++ app/themes/bootstrap/header.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Google Font -->
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,700|PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
