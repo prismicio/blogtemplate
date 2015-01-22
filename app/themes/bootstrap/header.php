a65eb9548a8e1cab4ab81fdbb5e67f461416db66
Erwan Loisant <eloisant@gmail.com>
2015-01-22 13:56:31 +0100

    Include modernizer for vw unit in css

--- app/themes/bootstrap/header.php
+++ app/themes/bootstrap/header.php
@@ -20,11 +20,8 @@
     <!-- Google Font -->
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!-- Modernizr -->
+    <script src="/app/static/modernizr.js"></script>
 
 </head>
 
