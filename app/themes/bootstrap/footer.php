cfe70f0e804c95b32859474ee640b5cea76512b3
Erwan Loisant <eloisant@gmail.com>
2015-01-26 09:43:53 +0100

    Sliding menu on the right, with hamburger icon

--- app/themes/bootstrap/footer.php
+++ app/themes/bootstrap/footer.php
@@ -4,9 +4,15 @@
         <a href="#">Back to top</a>
     </p>
 </footer>
-
+</div>
 <!-- Prismic toolbar -->
 <script src="//static.cdn.prismic.io/prismic.min.js"></script>
+
+<!-- Hamburger menu -->
+<script src="/app/static/jquery.panelslider.js"></script>
+<script type="text/javascript">
+    $('#menu-hamburger').panelslider({side: 'right', duration: 200 });
+</script>
 
 </body>
 </html>
--- app/themes/bootstrap/header.php
+++ app/themes/bootstrap/header.php
@@ -4,52 +4,45 @@
 <head>
     <title><?= single_post_title() ?> <?= site_title() ?></title>
     <link rel="alternate" type="application/rss+xml" title="<?= site_title() ?>'s Feed" href="/feed" />
-    <!-- Theme: WP Bootstrap -->
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/app/static/common.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/app/themes/bootstrap/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Blog Template -->
-    <link rel="stylesheet" href="/app/static/common.css">
-
-    <!-- Current theme -->
-    <link rel="stylesheet" href="/app/themes/bootstrap/main.css">
-
-    <!-- Google Font -->
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
-
-    <!-- Modernizr -->
     <script src="/app/static/modernizr.js"></script>
-
+    <script src="/app/static/jquery-1.11.2.min.js"></script>
 </head>
 
 <body class="<?= is_front_page() ? 'front-page' : '' ?>">
 
-    <nav class="blog-nav">
-        <li class="blog-nav-item"><?= home_link('Home') ?></li>
-        <?php foreach(get_pages() as $page) { ?>
-            <?php if(count($page['children']) > 0) { ?>
-                <li class="blog-nav-item dropdown">
-                    <?= page_link($page) ?>
-                    <ul class="dropdown-menu">
-                        <?php foreach($page['children'] as $subpage) { ?>
-                            <?= page_link($subpage) ?>
-                        <?php } ?>
-                    </ul>
-                </li>
-            <?php } else { ?>
-                <li class="blog-nav-item">
-                    <?= page_link($page) ?>
-                </li>
+    <div id="right-panel">
+        <ul>
+            <li>
+                <?php get_search_form() ?>
+            </li>
+            <li class="blog-nav-item"><?= home_link('Home') ?></li>
+            <?php foreach(get_pages() as $page) { ?>
+                <?php if(count($page['children']) > 0) { ?>
+                    <li class="blog-nav-item">
+                        <?= page_link($page) ?>
+                        <ul>
+                            <?php foreach($page['children'] as $subpage) { ?>
+                                <?= page_link($subpage) ?>
+                            <?php } ?>
+                        </ul>
+                    </li>
+                <?php } else { ?>
+                    <li class="blog-nav-item">
+                        <?= page_link($page) ?>
+                    </li>
+                <?php } ?>
             <?php } ?>
-        <?php } ?>
         </ul>
-    </nav>
-
-    <div class="navbar-form searchbar">
-        <?php get_search_form() ?>
     </div>
 
+<div id="main">
+    <a id="menu-hamburger" href="#right-panel">
+        <i class="fa fa-bars"></i>
+    </a>
 
