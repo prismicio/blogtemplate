aed572f00afa54cc8533b27593598b04f8275be4
Erwan Loisant <eloisant@gmail.com>
2015-01-27 10:26:20 +0100

    Fix icons

--- app/themes/default/content.php
+++ app/themes/default/content.php
@@ -4,10 +4,10 @@
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h2>
     <p class="blog-post-meta">
-        <span class="glyphicon glyphicon-time" aria-hidden="true"></span> <?= the_date_link() ?>
-        <span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?= the_author_link() ?>
-        <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span> <?php the_category(', ') ?>
-        <span class="glyphicon glyphicon-tags" aria-hidden="true"></span> <?php the_tags('', ', ') ?>
+        <span class="fa fa-calendar" aria-hidden="true"></span> <?= the_date_link() ?>
+        <span class="fa fa-user" aria-hidden="true"></span> <?= the_author_link() ?>
+        <span class="fa fa-folder-open" aria-hidden="true"></span> <?php the_category(', ') ?>
+        <span class="fa fa-tags" aria-hidden="true"></span> <?php the_tags('', ', ') ?>
     </p>
 
     <div class="post-thumbnail">
