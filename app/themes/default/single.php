75e84156723a7ac748f2ab274f7985c20d954ab6
Sebastien Renault <sre@zenexity.com>
2015-03-27 10:26:21 +0100

    Re-work on CSS blog post integration.

--- app/themes/default/single.php
+++ app/themes/default/single.php
@@ -3,14 +3,10 @@
 <?php while ( have_posts() ) : the_post(); ?>
 
 <div class="blog-header" style="background-image: url(<?= post_thumbnail_url() ?>)">
-    <div class="container">
-        <h1 class="blog-title"><?= single_post_title() ?></h1>
-    </div>
+    <h1 class="blog-title"><?= single_post_title() ?></h1>
 </div>
 
-<div class="container">
-    <div class="blog-main" <?= wio_post_attributes() ?>>
-
+<div class="blog-main container" <?= wio_post_attributes() ?>>
 
 <?php
 $post_year  = get_the_time('Y');
@@ -18,19 +14,14 @@ $post_month = get_the_time('m');
 $post_day   = get_the_time('d');
 ?>
 
-<p class="blog-post-meta">
-    <span class="fa fa-calendar" aria-hidden="true"></span> <?= get_day_link($post_year, $post_month, $post_day) ?>,
-    <span class="fa fa-calendar" aria-hidden="true"></span> <?= the_author_link() ?></p>
-
     <?php the_content() ?>
 
     <?php previous_post_link() ?>
 
     <?php next_post_link() ?>
 
-<?php endwhile; ?>
-
-        </div>
 </div>
+
+<?php endwhile; ?>
 
 <?php get_footer() ?>
