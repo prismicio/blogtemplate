7ddb8e774fbf499442a57e94533bac3a34e60020
Sebastien Renault <sre@zenexity.com>
2015-03-27 17:15:17 +0100

    Scope to blog post page underline & header mask.

--- app/themes/default/single.php
+++ app/themes/default/single.php
@@ -2,7 +2,7 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-<div class="blog-header" style="background-image: url(<?= post_thumbnail_url() ?>)">
+<div class="blog-header single" style="background-image: url(<?= post_thumbnail_url() ?>)">
     <div class="wrapper">
        <?php single_post_date() ?>
        <h1 class="blog-title"><?= single_post_title() ?></h1>
