1e9b1adff0ff2aa8a97e4800f9cc357c49ee0394
litig <litig@litigjr.(none)>
2015-04-02 16:00:38 +0200

    prismic.min.js must be served by prismic.io.

--- app/themes/default/single.php
+++ app/themes/default/single.php
@@ -30,7 +30,7 @@
 
 <!-- Prismic toolbar -->
 <script src="//www.google-analytics.com/cx/api.js?experiment=<?=current_experiment_id()?>"></script>
-<script src="//wroom.xxx/prismic.min.js"></script>
+<script src="//prismic.io/prismic.min.js"></script>
 <?php if(current_experiment_id()) { ?>
 <script>prismic.startExperiment("<?=current_experiment_id()?>", cxApi);</script>
 <?php }?>
