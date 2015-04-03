94961466c8f0903a5647d2f6f719f251755fe809
Sebastien Renault <srenault.contact@gmail.com>
2015-04-03 17:42:16 +0200

    Check if thread exists, if not create it, and then create a post.

--- app/static/disqium/disqium.js
+++ app/static/disqium/disqium.js
@@ -13,14 +13,22 @@ function Disqium(scope, disqus) {
         var endpoint = 'https://disqus.com/api/3.0';
         return {
             threads: {
-                create: function(identifier) {
+                create: function(title, identifier) {
                     var thread = {
-                        api_key: disqus.apiKey,
-                        forum: disqus.forum,
+                        title: title,
                         identifier: identifier
                     };
-                    var url = endpoint + '/threads/create.json?' + $.param(thread);
-                    return $.ajax({ url: url, type: 'POST' });
+                    var url = 'http://wroom.dev/starterkit/disqus/threads/create';
+                    return $.ajax({ url: url, type: 'POST', data: thread });
+                },
+                details: function(identifier) {
+                    var params = {
+                        api_key: disqus.apiKey,
+                        forum: disqus.forum,
+                        'thread:ident': identifier
+                    };
+                    var url = endpoint + '/threads/details.json?' + $.param(params);
+                    return $.ajax({ url: url, type: 'GET' });
                 }
             },
             posts: {
@@ -43,10 +51,10 @@ function Disqium(scope, disqus) {
         var $el = $(el);
         var $button = $('<button class="disqium-toggle-thread">X</button>');
         var $form = $('<form name="disqium-new-post">\
-                         <label for="message">Post:</label>\
-                         <input type="text" name="post" />\
-                         <input type="submit" value="Post"/>\
-                       </form>');
+                      <label for="message">Post:</label>\
+                      <input type="text" name="post" />\
+                      <input type="submit" value="Post"/>\
+                      </form>');
         var $wrapper = $('<div class="disqium-wrapper"></div>');
         $wrapper.append($button).append($form);
         $el.append($wrapper);
@@ -63,9 +71,18 @@ function Disqium(scope, disqus) {
         var $form = $(this);
         var text = $form.find('input[name=post]').val();
         e.preventDefault();
-        var threadIdentifier = $form.closest('[data-disqium-thread-id]').data('disqium-thread-id');
-        DisqusAPI.threads.create(threadIdentifier);
-        //DisqusAPI.posts.create('anonymous', 'anonymous@prismic.io', text, '3648676811');
+        var $paragraph = $form.closest('[data-disqium-thread-id]');
+        var identifier = $paragraph.data('disqium-thread-id');
+        DisqusAPI.threads.details(identifier).then(function(response) {
+            return response.response.id;
+        }).fail(function() {
+            var title = $paragraph.text().substring(0, 100);
+            return DisqusAPI.threads.create(title, identifier).then(function(response) {
+                return response.id;
+            });
+        }).then(function(threadId) {
+            DisqusAPI.posts.create('anonymous', 'anonymous@prismic.io', text, threadId);
+        });
     });
 
     $scope.find('[data-disqium-thread-id]').hover(function() {
