d3f93ad58ae5687e01693b271be68b534b361de6
Erwan Loisant <eloisant@gmail.com>
2015-04-09 14:27:23 +0200

    Page: remove title and use full width for slides

--- app/themes/default/page.php
+++ app/themes/default/page.php
@@ -2,24 +2,11 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 
-<div class="blog-header">
-    <div class="container">
-        <h1 class="blog-title"><?= the_title() ?></h1>
+    <div id="page-content">
+        <?php page_content() ?>
     </div>
-</div>
-
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12 blog-main">
-
-            <div id="page-content">
-                <?php page_content() ?>
-            </div>
 
 
-        </div>
-    </div>
-</div>
 <?php endwhile; // end of the loop. ?>
 
 <?php get_footer() ?>
