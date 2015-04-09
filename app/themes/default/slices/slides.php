4a8a9107430f577b0353c3a300b97f734088d822
Erwan Loisant <eloisant@gmail.com>
2015-04-09 14:45:55 +0200

    Style the slides

--- app/themes/default/slices/slides.php
+++ app/themes/default/slices/slides.php
@@ -5,17 +5,17 @@
     <?php $illustration = $groupDoc->get('illustration')->getMain(); ?>
     <?php $readMore = $groupDoc->get('read-more'); ?>
     <div class="slide" style="background-image: url(<?= $illustration->getUrl(); ?>)">
-        <?= $groupDoc->get('title')->asHtml() ?>
-        <?= $groupDoc->get('summary')->asHtml() ?>
-        <?php if ($readMore) {
-            $url = $linkResolver->resolve($readMore);
-            $title = $readMore->get('title');
-            echo '<a href="' . $url . '">' . $title . '</a>';
-        } ?>
-        <div class="slide-arrows">
-            <a href="#" class="arrow-prev">Previous</a>
-            <a href="#" class="arrow-next">Next</a>
+        <a href="#" class="arrow-prev">&nbsp;</a>
+        <div class="slide-container">
+            <?= $groupDoc->get('title')->asHtml() ?>
+            <?= $groupDoc->get('summary')->asHtml() ?>
+            <?php if ($readMore) {
+                $url = $linkResolver->resolve($readMore);
+                $title = $readMore->get('title');
+                echo '<a href="' . $url . '">' . $title . '</a>';
+            } ?>
         </div>
+        <a href="#" class="arrow-next">&nbsp;</a>
     </div>
 
 <?php } ?>
