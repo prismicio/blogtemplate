d637f34bf11a747e10dc46e8438863ec28a43c16
Sebastien Renault <srenault.contact@gmail.com>
2015-04-20 17:56:13 +0200

    Update prismic.js url to point to /snapshot/prismic.min.js

--- app/themes/default/prismic.php
+++ app/themes/default/prismic.php
@@ -5,7 +5,7 @@
   };
 </script>
 <script src="//www.google-analytics.com/cx/api.js?experiment=<?=current_experiment_id()?>"></script>
-<script src="//wroom.xxx/prismic.min.js"></script>
+<script src="//prismic.io/snapshot/prismic.min.js"></script>
 <?php if(current_experiment_id()) { ?>
   <script>prismic.startExperiment("<?=current_experiment_id()?>", cxApi);</script>
 <?php }?>
