c05e1f0e1aa054a6a9aaedcf3faf2158596909d5
Sebastien Renault <srenault.contact@gmail.com>
2015-04-24 14:54:07 +0200

    Style search page.

--- app/themes/default/search.php
+++ app/themes/default/search.php
@@ -1,20 +1,24 @@
 <?php get_header() ?>
-    <div class="blog-header home">
-    <div class="container">
-        <h1 class="blog-title"><?= site_title() ?></h1>
-        <p class="lead blog-description"><?= site_description() ?></p>
-    </div>
-    </div>
+
+<div class="blog-header search">
+
+  <div class="wrapper">
+
+    <h1>Query: <?= get_search_query() ?></h1>
+
+  </div>
+</div>
 
 <div class="container blog-main">
 
-Query: <?= get_search_query() ?>
-
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
     <?php get_template_part('content'); ?>
+
 <?php endwhile; else : ?>
+
     <p>Sorry, no posts matched your criteria.</p>
+
 <?php endif; ?>
 
 
