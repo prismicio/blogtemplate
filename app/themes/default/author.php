8d1b1802a71197d363bdfed92e51789bb089f41c
Sebastien Renault <srenault.contact@gmail.com>
2015-04-24 14:54:07 +0200

    Style search page.

--- app/themes/default/author.php
+++ app/themes/default/author.php
@@ -1,16 +1,27 @@
 <?php get_header() ?>
 
 <div class="blog-header author">
+
     <div class="wrapper">
+
         <div class="author-image" style="background-image: url(<?= single_author_image_url() ?>)"></div>
+
         <h1 class="blog-title author-name"><?= single_author_name() ?></h1>
+
         <div class="author-bio">
+
             <?= single_author_bio() ?>
+
         </div>
+
         <div class="author-sites">
+
             <?= single_author_links() ?>
+
         </div>
+
     </div>
+
 </div>
 
 <div class="blog-main container author">
