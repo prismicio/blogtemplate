a72f6ee23e72bfaa84f7b1e7ab2efef3e2d8958e
Sebastien Renault <srenault.contact@gmail.com>
2015-04-27 15:46:57 +0200

    Be able to add web fonts from prismic.

--- app/themes/default/pagetheme.php
+++ app/themes/default/pagetheme.php
@@ -1,9 +1,17 @@
 
+<?php $theme = the_theme(); ?>
+
+<?php if($theme) : ?>
+
+<?php foreach($theme->getGroup('theme.fonts')->getArray() as $item) { ?>
+
+  <link href='<?= $item->getLink("font")->getUrl() ?>' rel='stylesheet' type='text/css'>
+
+<?php } ?>
+
 <style>
 
 <?php
-
-  $theme = the_theme();
 
   $textColor = $theme->getColor('theme.page-text-color');
 
@@ -122,3 +130,9 @@ h2, h3 {
 }
 
 </style>
+
+<?php else: ?>
+
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
+
+<?php endif ?>
