651574ebad37a50e58b1310c30526a26b688ea3c
Sebastien Renault <srenault.contact@gmail.com>
2015-04-28 11:27:14 +0200

    search page: display total results.

--- app/themes/default/homeblog.php
+++ app/themes/default/homeblog.php
@@ -15,9 +15,13 @@
 <div class="container blog-main">
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
     <?php get_template_part('content'); ?>
+
 <?php endwhile; else : ?>
+
     <p>Sorry, no posts matched your criteria.</p>
+
 <?php endif; ?>
 
 <?php previous_posts_link() ?>
