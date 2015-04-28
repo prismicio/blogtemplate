37a8e8e2f1ad34bc6e59c738276e7ea0c0322eaa
Sebastien Renault <srenault.contact@gmail.com>
2015-04-28 12:10:19 +0200

    Fix tests

--- tests/WPTest.php
+++ tests/WPTest.php
@@ -86,7 +86,7 @@ class WPTest extends LocalWebTestCase
         $this->client->get('/sample-page');
         $this->assertEquals(200, $this->client->response->status());
         $html = str_get_dom($this->client->response->body());
-        $this->assertEquals('Sample Website Starter Page', trim($html('h2', 0)->getPlainText()));
+        $this->assertEquals('Website Starter Sample Page', trim($html('h2', 0)->getPlainText()));
     }
 
     public function testTag()
